34230a201b99ab12490d969ba3aef50a2e076280
unknown <soumya.ricky19@gmail.com>
2017-11-30 20:50:12 -0600

    changed color

--- index.php
+++ index.php
@@ -5,12 +5,15 @@
         // $last_line = exec('python lyrics_prediction.py "'.$mode.'" "'.$text.'"', $retval);
         
         // $text = preg_replace("/'/", "", $text);
-        file_put_contents("temp.txt",$text);
-        // echo $text
-        // $mode="songdata.csv";
-        $cmd="python lyrics_prediction.py ";//.$mode;
-        // echo $cmd;
-        $last_line = exec($cmd, $retval);
-        echo $last_line;
+        if ($text != ""){
+                file_put_contents("temp.txt",$text);
+                // echo $text
+                // $mode="songdata.csv";
+                $cmd="python lyrics_prediction.py ";//.$mode;
+                // echo $cmd;
+                $last_line = exec($cmd, $retval);
+                echo $last_line;
+        }
+        
         // echo "hi";
 ?>
